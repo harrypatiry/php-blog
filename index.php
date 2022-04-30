b175b26ef4d02c3db5a562bf1a2bc9882e9f102f
harrypatiry <harrypatiry@gmail.com>
2022-04-30 13:13:36 -0400

    working

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
 INCLUDE("security.php");
-INCLUDE("addPost.php");
-post();
 if(security_validate()){
    security_login();
 }
@@ -19,12 +17,44 @@ if(security_validate()){
                <button><a href='signup.php'>Sign Up</a></button>
                <button><a href='login.php'>Login</a></button>
                <?php else: ?>
-                  <div class="container">
+                  <div>
                      <h1>Logged In</h1>
-                     
-                     <div class='post-container'>
                         <button><a href='post.php'>Create A Post</a></button>
-                     </div>
+                        <div>
+                           <?php 
+                           global $connection;
+                           
+                           // Server
+                           $server = "localhost";
+                           // Username
+                           $username = "root";
+                           // If using XAMPP, 
+                           //  the password is an empty string.
+                           $password = "";
+                           // Database
+                           $database = "lab";
+                           // Use the global connection
+                           $connection = mysqli_connect($server, $username, $password, $database);
+                           if($connection == null) {
+                              $connection = mysqli_connect($server, $username, $password, $database);
+                           }
+                           
+                           $sql = "SELECT * FROM posts";
+                           $result = mysqli_query($connection, $sql);
+                           
+                           if(isset($_POST["new_post"])) {
+                              $title = $_POST["title"];
+                              $text = $_POST["text"];
+                              $sql = "INSERT INTO posts(title, text) VALUES('$title', '$text')";
+                              mysqli_query($connection, $sql);
+                           }
+                           while ($row = mysqli_fetch_assoc($result)) {
+                              echo "<div>";
+                              echo "<h1>" . $row['title'] . "</h1>"; 
+                              echo "<p>" . $row['text'] . "</p>"; 
+                              echo "</div>";
+                           } ?>
+                        </div>
                      <footer>
                         <button><a href='logout.php'>Log Out</a></button>
                         <button><a href='update.php'>Change Your Password</a></button>
--- post.php
+++ post.php
@@ -1,6 +1,5 @@
 <?php
-    INCLUDE("addPost.php");
-    post();
+INCLUDE "index.php"
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,10 +11,9 @@
 </head>
 <body>
     <form method="POST">
-        <input type="text" name="title" placeholder="title"/>
-        <textarea name="text"></textarea>
-        <button name="new_post">Create Post</button>
+            <input type="text" name="title" placeholder="title"/>
+            <textarea name="text"></textarea>
+            <button name="new_post">Create Post</button>
     </form>
-    <p>To see your post go <a href="index.php?info=added">home</a></p>
 </body>
 </html>
